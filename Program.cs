821eed3f6fa77f115c8cc8ced50ab332811b6fe2
IGShilyaev <55237654+IGShilyaev@users.noreply.github.com>
2020-05-28 12:54:53 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pract_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("f(x) = ||x|-1|");
+            int i = 0;
+            do
+            {
+                Console.Write("Введите число а: ");
+                double xValue = EnterDoubleNumber();
+                double yValue = ValueOfFunction(xValue);
+                Console.WriteLine($"f(a) = {yValue}");
+                i++;
+            } while (i < 10);
+        }
+
+        static double EnterDoubleNumber()
+        {
+            bool ok = false;
+            double n;
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out n);
+                if (!ok) { Console.Write("Ошибка! Не удалось преобразовать введенное значение в вещественное число. Введите другое значение: "); }
+            } while (!ok);
+            return n;
+        }
+
+        static double ValueOfFunction(double x)
+        {
+            return Math.Abs(Math.Abs(x) - 1);
+        }
+
+    }
+}
